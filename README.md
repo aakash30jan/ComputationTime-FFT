575be1dd1a611a54aad5e94e6e0681e2f2556f54
aero <aakash30jan@gmail.com>
2018-03-18 02:11:14 +0100

    python methods and data/plot

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Comparative study of computation time for different FFT methods and libraries.
 1. For Python, 'fft_computation_time.data' from is <a href="https://stackoverflow.com/questions/6365623/improving-fft-performance-in-python">here</a>.
 2. For FORTRAN, [N.A.]
 3. For C++, [N.A.]
+
--- generate_plots.py
+++ generate_plots.py
@@ -9,9 +9,9 @@ labels=['padded_fft','numpy_fft', 'scipy_fft', 'czt','fftw_fft']
 markers=['-s','-o','-v','-x','-*']
 colors=['r','k','b','g','y']
 
-my_xticks = ['20000 prime=False','20011 prime=True','21803 prime=True','21804 prime=False','21997 prime=True','32768 prime=False']  
-xv=([x[0],x[5],x[10],x[15],x[20],x[25])
-plt.xticks(xv, my_xticks)
+#my_xticks = ['20000 prime=False','20011 prime=True','21803 prime=True','21804 prime=False','21997 prime=True','32768 prime=False']  
+xv=(x[0],x[5],x[10],x[15],x[20],x[25])
+#plt.xticks(xv, my_xticks)
 
 for i in range(0,5):
         #plt.plot((x[0+i],x[5+i],x[10+i],x[15+i],x[20+i],x[25+i]),(y[0+i],y[5+i],y[10+i],y[15+i],y[20+i],y[25+i]),markers[i],label=labels[i])
@@ -22,5 +22,5 @@ plt.legend()
 plt.ylabel("Computation Time (s)")        
 plt.xlabel("Number of Elements")
 plt.title("FFT Computation Time")
-plt.savefig("FFT_Computation_Time_v1.eps")
+plt.savefig("FFT_Computation_Time_v1.pdf")
 plt.show()
